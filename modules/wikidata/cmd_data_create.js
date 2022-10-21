04fb2b30712c15aeca343ee4940f431ecc1c65e0
fmnb0516 <fmnb0516@gmail.com>
2022-10-21 11:14:04 +0900

    update

---
+++ modules/wikidata/cmd_data_create.js
@@ -0,0 +1,13 @@
+
+const run = async () => {
+    const application = await require("../../application")(["wikidata"], {discord:false, twitter:false});
+    
+    const key = process.argv[2];
+    if(key === undefined || key === null || key.trim() === "") {
+        return;
+    }
+    const data = await application.wikidata.callApi("load", [key.trim()]);
+    console.log(JSON.stringify(data, null , "\t"));
+};
+
+run();
