99cc452cd74e449352b3c12ceaf339f334aa45f9
fmnb0516 <fmnb0516@gmail.com>
2022-11-09 10:42:44 +0900

    update

--- modules/info/index.js
+++ modules/info/index.js
@@ -2,5 +2,39 @@
 module.exports = async (parameter) => {
     const builder = parameter.instance();
 
+    const infos = await parameter.lib.fs.readdir(parameter.moduleDir + '/data');
+
+
+    const parseCount = (data, def) => {
+        if(data === undefined || data === null || (typeof data) !== 'number') {
+            return def;
+        }
+        return data > 0 ? data : def;
+    };
+
+    builder.addApi('get', async (count) => {
+        const paramCount = parseCount(count, infos.length);
+        const loopCount = paramCount > infos.length ? infos.length : paramCount;
+
+        const result = [];
+
+        for (let i = 0; i < loopCount; i++) {
+            const path = infos[i];
+
+            const textData = await parameter.lib.fs.readFile(parameter.moduleDir + '/data/' + path, 'utf-8');
+            result.push(textData);
+        }
+
+        return result.map(text => {
+            const entries = text.split("\r\n");
+            return {
+                title : entries[0].trim(),
+                description : entries[1].trim(),
+                data : entries.slice(3).join("\r\n")
+            };
+        });
+    });
+
+
     return builder;
 };
