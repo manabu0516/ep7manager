3c4588d0f6562c48f82467f8389cde9273a9331c
fmnb0516 <fmnb0516@gmail.com>
2022-11-10 18:06:21 +0900

    update.

--- modules/wikidata/cmd_data_create.js
+++ modules/wikidata/cmd_data_create.js
@@ -1,16 +1,6 @@
 
 const run = async () => {
     const application = await require("../../application")(["wikidata"], {discord:false, twitter:false});
-    
-    const key = process.argv[2];
-    if(key === undefined || key === null || key.trim() === "") {
-        return;
-    }
-    const data = await application.wikidata.callApi("load", [key.trim(), {
-        force : true
-    }]);
-
-    console.log(JSON.stringify(data, null , "\t"));
 };
 
 run();
--- modules/wikidata/index.js
+++ modules/wikidata/index.js
@@ -1,223 +1,88 @@
 
-const loadHeroData = async (key, url , force, context) => {
+
+const resolveData = async (alias, option, context) => {
     const jsonDir = context.publicDir + '/json/hero';
-    const dataFilePath = jsonDir + '/' + key + '.json';
+    const dataFilePath = jsonDir + '/' + alias["_path"]; 
 
-    if(force === false && await fileExist(context.fs, dataFilePath) == true) {
-        const text = await context.fs.readFile(dataFilePath, 'utf8');
-        return JSON.parse(text);
-    }
-
-    const response = await context.request(url);
-    const $ = context.cheerio.load(response);
-
-    const image = $(".archive-style-wrapper .a-paragraph img").attr('data-src');
-
-    const common = {};
-    const commonElementTable = findElement($, $('h3.a-header--3'), key+'の基本情報');
-    common["名前"] = key;
-    common["レアリティ"] = $(commonElementTable.find("tr").get(3)).find("td div a").text();
-    common["属性"] = $(commonElementTable.find("tr").get(4)).find("td div a").text();
-    common["職業"] = $(commonElementTable.find("tr").get(5)).find("td div a").text();
-
-    const status = {};
-    const statusElementTable = findElement($, $('h3.a-header--3'), '最大ステータス【入手時 / 覚醒後】');
-    statusElementTable.find("tr").each((idx, elm) => {
-        const key = $($(elm).find('th').get(0)).text().trim();
-        if(key === '-') {
-            return;
-        }
-
-        const init_val = $($(elm).find('td').get(0)).text().trim();
-        const max_val = $($(elm).find('td').get(1)).text().trim();
-        status[key] = {'初期値':init_val, '最大値':max_val};
-    });
-
-    const formation = {
-        '刻印展開' : [],
-        '刻印集中' : [],
-    };
-
-    const formationElementTable = findElement($, $('h3.a-header--3'), '陣形効果');
-    formationElementTable.find("tr").each((idx, elm) => {
-
-        const f1 = $($(elm).find('td').get(0)).text().trim();
-        const f2 = $($(elm).find('td').get(1)).text().trim();
-        if(f1 === '' || f2 === '') {
-            return;
-        }
-
-        const f1_entry = f1.split("+");
-        const f1_data = {};
-        f1_data[f1_entry[0]] = f1_entry[1];
-        formation["刻印展開"].push(f1_data);
-
-        const f2_entry = f2.split("+");
-        const f2_data = {};
-        f2_data[f2_entry[0]] = f2_entry[1];
-        formation["刻印集中"].push(f2_data);
-    });
-
-    const skil1ElementTable = findElement($, $('h3.a-header--3'), 'スキル');
-    const skil2ElementTable = skil1ElementTable.next();
-    const skil3ElementTable = skil2ElementTable.next();
-
-    const skills = [];
-
-    [skil1ElementTable, skil2ElementTable, skil3ElementTable].forEach(element => {
-        const skillName = $($(element).find("tr").get(0)).find("th").text().trim();
-        const skillDesc = $($(element).find("tr").get(0)).find("td").text().trim().replace("【スキル効果】", "");
-
-        const strengthen = $($(element).find("tr").get(1)).find("td").contents();
-        const strengthenData = [];
-        for (let i = 0; i < strengthen.length; i++) {
-            const element = strengthen[i];
-            if(element.type !== "text") {
-                continue;
-            }
-
-            const text = element.data.trim();
-            if(text === "") {
-                continue;
-            }
-            strengthenData.push(text.split("：")[1].trim());
-        }
-
-        const materialsData = [];
-
-        const materials = $($(element).find("tr").get(2)).find("td div");
-        for (let i = 0; i < materials.length; i++) {
-            const element = $(materials[i]);
-            const text = element.text().split("：")[1].trim();
-            const entries = text.split("/");
-            
-            const listData = {};
-
-            entries.map(e => e.split("×")).forEach(pair => {
-                const m_name = pair[0].trim();
-                const m_count = pair[1].trim();
-                listData[m_name] = m_count;
-            });
-
-            materialsData.push(listData);
-        }
-
-        skills.push({
-            "スキル名":skillName,
-            "説明":skillDesc.split("\n").map(t => t.trim()).filter(t => t != ''),
-            "強化効果":strengthenData,
-            '強化素材':materialsData
-        });
-    });
-
-    const result = {
-        "画像" : image,
-        "共通" : common,
-        "ステータス" : status,
-        "陣形効果" : formation,
-        'スキル': skills
-    };
-
-    const json = JSON.stringify(result, null , "\t");
-    
-    await context.fs.mkdir(jsonDir, { recursive: true });
-    await context.fs.writeFile(dataFilePath, json, 'utf8');
-
-    return result;
+    const text = await context.fs.readFile(dataFilePath, 'utf8');
+    return JSON.parse(text);
 };
 
-const fileExist = async (fs, path) => {
-    try {
-        await fs.lstat(path);
-        return true;
-    } catch(e) {
-        return false;
-    }
-};
-
-const findElement = ($, selector, search) => {
-    let result = null;
-    $(selector).each((idx, elem) => {
-        const text = $(elem).text().trim();
-
-        if(text === search) {
-            result = $(elem).next();
-            return false;
-        }
-    });
-    return result != null ? result : null;
-};
-
-class AliasBuilder {
+class AliasManager {
     constructor() {
-        this.result = {};
-        this.target = null;
+        this.datamap = {};
     };
 
-    handle(data) {
-        if(data.startsWith("+")) {
-            this.target = data.substring(1);
-        } else if(this.target != '') {
-            this.result[data] = this.target;
+    load(json) {
+        const keys = Object.keys(json);
+        for (let i = 0; i < keys.length; i++) {
+            const key = keys[i];
+            const data = json[key];
+
+            data["_name"] = this.key;
+            data["_path"] = this.normalize(this.key) + '.json';
+
+            for (let j = 0; j < data.alias.length; j++) {
+                const alias = data.alias[j];
+                this.datamap[alias] = data;           
+            }
+
+            const localize = Object.keys(data.localize);
+            for (let j = 0; j < localize.length; j++) {
+                const locale = localize[j];
+                const normalizeKey = this.normalize(localize[locale]);
+                if(normalizeKey === "") {
+                    continue;
+                }
+                this.datamap[normalizeKey] = data;
+            }
         }
     };
 
-    get() {
-        return this.result;
-    };
+    normalize(text) {
+        return text.replace(" ", "").toLocaleLowerCase().trim();
+    }
+
+    get(key) {
+        const normalizeKey = this.normalize(key);
+        return this.datamap[normalizeKey];
+    }
+
+    entries() {
+        return Object.keys(this.datamap);
+    }
 };
 
 module.exports = async (parameter) => {    
     const builder = parameter.instance();
 
-    const listFile = await parameter.lib.fs.readFile(parameter.moduleDir + '/list.txt', 'utf8');
-    const aliasFile = await parameter.lib.fs.readFile(parameter.moduleDir + '/alias.txt', 'utf8');
+    const aliasJson = JSON.parse(await parameter.lib.fs.readFile(parameter.moduleDir + '/alias.json', 'utf8'));
+    const aliasData = new AliasManager().load(aliasJson);
 
-    const aliasData = new AliasBuilder();
-    aliasFile.split("\r\n").forEach(e => {
-        const data = e.trim();
-        if(data === '') {
-            return;
-        }
-
-        aliasData.handle(data);
-    });
-    const aliasMap = aliasData.get();
-
-    const entires = {};
-    listFile.split('\n').map(line => line.trim().split(',')).forEach(pair => {
-        if(pair[0].startsWith("#") === true || pair[0] === '') {
-            return;
-        }
-        entires[pair[0]] = pair[1];
-    });
-
-    const listData = Object.keys(entires).map(e => {
-        return e + '.json';
-    });
+    const listData = aliasData.entries().map(e => e + '.json');
 
     const jsonp = '__ep7manager_load_hero(' + JSON.stringify(listData) + ', []);';
     await parameter.lib.fs.writeFile(parameter.publicDir + '/jsonp.js', jsonp, 'utf-8');
 
     builder.addApi('entries', () => {
-        return Object.keys(entires);
+        return aliasData.entries();
     });
 
     builder.addApi('normalize', (key) => {
-        const data = aliasMap[key];
-        return data ? data : key;
+        const data = aliasData.get(key);
+
+        return data != undefined ? data._name : key;
     });
 
     builder.addApi('load', async (key, option) => {
         option = option ? option : {};
 
-        const url = entires[key];
-        if(url === undefined || url === null) {
+        const alias = aliasData.get(key);
+        if(alias === undefined || alias === null) {
             return null;
         }
-        const data = await loadHeroData(key, url, option.force === true, {
-            request : parameter.lib.request,
-            cheerio : parameter.lib.cheerio,
+        
+        const data = resolveData(alias, option, {
             fs : parameter.lib.fs,
             publicDir : parameter.publicDir
         });
