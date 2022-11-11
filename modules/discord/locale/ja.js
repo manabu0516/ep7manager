5fb0f69aeebf94b759f14a4f483551cc6e3b128f
fmnb0516 <fmnb0516@gmail.com>
2022-11-12 02:28:27 +0900

    update.

--- modules/discord/locale/ja.js
+++ modules/discord/locale/ja.js
@@ -59,7 +59,30 @@ module.exports = {
     st_label_clazz: (key) => clazzDef[key] === undefined ? key : clazzDef[key],
     st_label_type: (key) => typeDef[key] === undefined ? key : typeDef[key],
     st_label_soul: (key) => key.replace("soul", "魂力").replace("focus", "集中").replace("consume", "闘志"),
-    st_label_enhance_type : (key) => key,
+    st_label_enhance_type : (key) => key.replace("damage received", "被ダメージ").replace("damage dealt", "ダメージ")
+        .replace("Health recovered when revived", "復活時の生命力")
+        .replace("effect chance", "効果発生率")
+        .replace("max Health", "最大生命力")
+        .replace("turn cooldown", "スキルターン")
+        .replace("Combat Readiness", "アクションゲージ")
+        .replace("Attack increase", "攻撃力アップ")
+        .replace("all stats", "各ステータス")
+        .replace("stat increase", "追加ステータス")
+        .replace("counterattack chance", "反撃発生率")
+        .replace("trigger chance", "発動率")
+        .replace("activation chance", "発生率")
+        .replace("by additional skill", "(追加スキル)")
+        .replace("barrier strength", "シールド効果")
+        .replace("Critical Hit Resistance", "クリティカル低効率")
+        .replace("healing", "回復量")
+        .replace("Attack", "攻撃力")
+        .replace("Consume", "消費量")
+        .replace("Acquire", "獲得量")
+        .replace("acquired", "獲得アップ")
+        .replace("decrease", "ダウン効果")
+        .replace("Soul", "魂力")
+        .replace("Additional", "追加")
+        .replace("Fighting Spirit", "闘志"),
 
     info_label_1 : () => "内容",
 };
