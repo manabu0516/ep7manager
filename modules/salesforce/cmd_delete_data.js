a538eec1a95c1182f62566d5f5d480408cd4c4d0
fmnb0516 <fmnb0516@gmail.com>
2023-01-05 15:33:16 +0900

    update

--- modules/salesforce/cmd_delete_data.js
+++ modules/salesforce/cmd_delete_data.js
@@ -3,7 +3,7 @@ const run = async () => {
     const application = await require("../../application")(["salesforce"], {discord:false, twitter:false});
 
     const password = process.argv[2];
-    const dataid = process.argv[1];
+    const dataid = process.argv[3];
     const data = await application.salesforce.callApi("delete", [password, dataid]);
     console.log(data);
 };
