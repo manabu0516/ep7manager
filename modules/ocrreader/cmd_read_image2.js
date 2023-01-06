22a84b357ee74449ccc9daacc58d9908c47543e0
fmnb0516 <fmnb0516@gmail.com>
2023-01-06 09:18:11 +0900

    update

---
+++ modules/ocrreader/cmd_read_image2.js
@@ -0,0 +1,17 @@
+
+const run = async () => {
+    const application = await require("../../application")(["ocrreader", "wikidata"], {discord:false, twitter:false});
+    const target = application.ocrreader;
+
+    const imagePath = process.argv[2];
+    const data = await application._context.lib.fs.readFile(imagePath);
+    
+    const result = await target.callApi("recognize", [data, {
+        sliceCount : 0,
+        dataCount : 9
+    }]);
+    console.log(result);
+
+};
+
+run();
